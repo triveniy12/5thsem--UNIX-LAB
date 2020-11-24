7c1259a9bde3cb4ad82032a1c3f1f6d80b50dbc5
triveniy12 <55705306+triveniy12@users.noreply.github.com>
2020-11-24 15:23:20 +0530

    Delete head-tail.sh

--- Lab (24-11-2020)/head-tail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo " Enter the first file:"
-read f1
-echo "Enter the second file:"
-read f2
-head -n 5 $f1 | cat > f3
-tail -n 8 $f2 | cat >> f3
-echo "The contents of newly created file"
-cat f3
